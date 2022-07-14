f4093cffd8e1b0ee8163fd871cadd0471d7a7d32
EmelKesten <kestenemel@gmail.com>
2022-07-14 19:22:24 +0200

    commit

--- react-app-the-movies/src/App.js
+++ react-app-the-movies/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import NavBar from './Navbar.js';
+import GetPopularMovies from './MostPopular';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <div class="content">
-        <div id="search" class="layout"></div>
-        <div id="popUp" class="noShow"></div>
-        <div id="mostPopular"></div>
-        <div id="nowPlaying"></div>
-        <div id="upComing"></div>
-      </div>
+      <GetPopularMovies></GetPopularMovies>
     </div>
   );
 }
